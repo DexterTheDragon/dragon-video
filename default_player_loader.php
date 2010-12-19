e7883d74505a5c8a62e8fa0bca271afa0013f17d
Kevin Carter <cartkev@gmail.com>
2010-12-19 11:51:40 -0700

    Add default loader plugins

---
+++ default_player_loader.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Dragon Video - VideoJS Player
+Plugin URI: http://github.com/DexterTheDragon/dragon-video
+Description: Bundled player for Dragon Video. Uses the http://videojs.com player.
+Author: Kevin Carter
+Version: 0.1
+Author URI: http://dexterthedragon.com/
+*/
+require_once 'players/videojs/player.php';
