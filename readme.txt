e6067cd2b8691fe3b51391b273d777db6c1d8702
Kevin Carter <cartkev@gmail.com>
2013-07-09 17:48:51 -0600

    Version 1.0.1

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: dexterthedragon
 Tags: html5, video
 Requires at least: 3.5
 Tested up to: 3.6
-Stable tag: 1.0
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- videojs-player.php
+++ videojs-player.php
@@ -5,7 +5,7 @@ Plugin URI: http://github.com/DexterTheDragon/dragon-video
 Description: Bundled player for Dragon Video. Uses the http://videojs.com player.
 Author: Kevin Carter
 Author URI: http://dexterthedragon.com/
-Version: 1.0.0
+Version: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
